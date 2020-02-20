a6d52d0e06c6f381d3112ae7510732ca3ab16286
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-20 11:23:36 -0500

    setting state correctly depending on old state

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,10 +20,6 @@ class App extends React.Component {
   componentDidMount() {
     console.log('[App.js] componentDidMount');
   }
-  //@deprecated
-  // componentWillMount() {
-  //   console.log('[App.js] componentWillMount');
-  // }
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[App.js] shouldComponentUpdate');
@@ -41,7 +37,8 @@ class App extends React.Component {
       { name: 'Cholo', age: 25, id: 3 }
     ],
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   nameChangedHandler = (event, id) => {
@@ -53,8 +50,11 @@ class App extends React.Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({
-      persons
+    this.setState((prevState, props) => {
+      return {
+        persons,
+        changeCounter: prevState.changeCounter + 1
+      };
     });
   };
 
@@ -102,59 +102,7 @@ class App extends React.Component {
         {persons}
       </Aux>
     );
-    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
   }
 }
-
-// const App = props => {
-//   const [personsState, setPersonsState] = React.useState({
-//     persons: [
-//       { name: 'Carlos', age: 28 },
-//       { name: 'Azusa', age: 26 },
-//       { name: 'Cholo', age: 25 }
-//     ]
-//   });
-
-//   const [otherState] = React.useState('some other value');
-
-//   console.log(personsState, otherState);
-
-//   const switchNameHandler = () => {
-//     console.log('I was Clicked!');
-//     // DON'T DO THIS: this.state.persons['0'].name = 'Carlos Andres';
-//     setPersonsState({
-//       ...personsState,
-//       persons: [
-//         { name: 'Carlos Andres', age: 28 },
-//         { name: 'Azusa', age: 26 },
-//         { name: 'Cholo', age: 25 }
-//       ]
-//     });
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>Hi, I'm a React App</h1>
-//       <p>This is really working!</p>
-//       <button onClick={switchNameHandler}>Switch Name</button>
-//       <Person
-//         name={personsState.persons[0].name}
-//         age={personsState.persons[0].age}
-//       />
-//       <Person
-//         name={personsState.persons[1].name}
-//         age={personsState.persons[1].age}
-//       >
-//         {' '}
-//         My Hobbies: Play Piano{' '}
-//       </Person>
-//       <Person
-//         name={personsState.persons[2].name}
-//         age={personsState.persons[2].age}
-//       />
-//     </div>
-//   );
-//   // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
-// };
 
 export default withClass(App, 'App');
